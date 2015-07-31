fb08bb2cbeeab2ba4640d70de11f8663689ecc6a
Matthew Suozzo <matthew.suozzo@gmail.com>
2015-07-31 19:14:21 -0400

    Initial commit

---
+++ reader.py
@@ -0,0 +1,20 @@
+"""Scrape Kindle Cloud Reader for information regarding the Kindle Library and
+current reading progress.
+"""
+
+
+def get_current_progress(dummy_book):
+    """Return the current progress as a 2-tuple of locations: (current, total)
+    """
+    #TODO
+    pass
+
+def get_library():
+    """Return the list of books in the kindle library
+    """
+    #TODO
+    pass
+
+
+if __name__ == "__main__":
+    print get_library()
