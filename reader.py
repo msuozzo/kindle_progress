546f4f6dd99c5096e922d06537e604f56bfa72a7
Matthew Suozzo <matthew.suozzo@gmail.com>
2015-08-05 18:03:34 -0400

    Fixed several race-condition errors in current_progress

--- reader.py
+++ reader.py
@@ -6,11 +6,15 @@ from credential_mgr import JSONCredentialManager
 import sys
 import os
 from getpass import getuser
+import re
 
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver import Firefox, ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException,\
+                                        ElementNotVisibleException,\
+                                        WebDriverException
 
 
 class ConnectionError(Exception):
@@ -81,28 +85,85 @@ class KindleCloudReader(object):
     def __init__(self, amz_login_credentials_path, profile_path=None):
         if profile_path is not None:
             profile = FirefoxProfile(profile_path)
+        else:
+            profile = None
         self._browser = Firefox(firefox_profile=profile)
-        self._wait = WebDriverWait(self._browser, 10)
+        self._wait = WebDriverWait(self._browser,
+                timeout=10,
+                ignored_exceptions=(NoSuchElementException,
+                    ElementNotVisibleException))
         self._manager = JSONCredentialManager(amz_login_credentials_path)
         self._action = ActionChains(self._browser)
 
     def _to_reader_home(self):
         """Navigate to the Cloud Reader library page
         """
+        # NOTE: Prevents QueryInterface error caused by getting a URL
+        # while switched to an iframe
+        self._browser.switch_to_default_content()
         self._browser.get(KindleCloudReader.CLOUD_READER_URL)
+
         if self._browser.title == u'Problem loading page':
             raise ConnectionError
-        elif self._browser.title == u'Amazon.com Sign In':
+
+        # Wait for either the login page or the reader to load
+        login_or_reader_loaded = \
+                lambda br: br.find_elements_by_id('amzn_kcr') or \
+                    br.find_elements_by_id('KindleLibraryIFrame')
+        self._wait.until(login_or_reader_loaded)
+
+        # If the login page was loaded, log in
+        if self._browser.title == u'Amazon.com Sign In':
             self._login()
 
-        # Wait for cloud reader to be loaded
+        assert self._browser.title == u'Kindle Cloud Reader'
+
         self._switch_to_frame('KindleLibraryIFrame')
+        self._sync_library()
 
-        # Sync Cloud Reader
-        self._sync()
+    def _to_book_reader(self, book):
+        """Navigate to the Cloud Reader page for `book`
+        """
+        # Go to cloud reader home
+        self._to_reader_home()
 
-    def _sync(self):
-        """Sync the cloud reader
+        self._wait.until(lambda br: br.find_element_by_id(book.id_))
+
+        def _find_and_click(browser):
+            """Finds the book, scrolls it into view, and clicks on it
+            """
+            title_div = browser.find_element_by_id(book.id_)\
+                                    .find_element_by_class_name('book_title')
+            scroll_into_view(title_div)
+            title_div.click()
+            return True
+        self._wait.until(_find_and_click)
+
+        # Switch to Reader frame
+        self._browser.switch_to_default_content()
+        self._switch_to_frame('KindleReaderIFrame')
+        self._sync_reader()
+
+    def _sync_reader(self):
+        """Sync the reader position to the furthest available
+        """
+        # Sync page to furthest position if further position was found
+        # NOTE: book_container indicates the reader loaded without sync alert
+        sync_btn_id = 'kindleReader_dialog_syncPosition_sync_btn'
+        sync_or_read = lambda br: br.find_elements_by_id(sync_btn_id) or \
+                br.find_elements_by_id('kindleReader_book_container')
+        self._wait.until(sync_or_read)
+
+        # Check whether sync is required
+        sync_btn_elems = self._browser.find_elements_by_id(sync_btn_id)
+        if sync_btn_elems:
+            sync_btn = sync_btn_elems[0]
+            if sync_btn.is_displayed():
+                sync_btn.click()
+            self._wait.until_not(lambda br: sync_btn.is_displayed())
+
+    def _sync_library(self):
+        """Sync the Kindle Cloud Library
         """
         if not self._browser.current_url.startswith(KindleCloudReader.CLOUD_READER_URL):
             raise RuntimeError('current url "%s" is not a cloud reader url ("%s")' %
@@ -159,11 +220,16 @@ class KindleCloudReader(object):
             title = title_elem.text
             author = container.find_element_by_class_name('book_author').text
             books.append(KindleBook(id_, title, author))
+
         return books
 
     def get_current_progress(self, book):
         """Return the current progress as 2 2-tuples:
             ((curr_page, total_pages), (curr_location, total_locations))
+        NOTE: If either the page numbers or location information are not
+            available, None will be returned in place of the corresponding
+            tuple.
+                e.g. (None, (curr_loc, total_loc))
 
         Notes on Progress Formats:
 
@@ -189,39 +255,36 @@ class KindleCloudReader(object):
         Args:
             read_cb: The callback to open the reader for the target book
         """
-        # Go to cloud reader home
-        self._to_reader_home()
-
-        self._wait.until(lambda br: br.find_element_by_id(book.id_))
-        book_div = self._browser.find_element_by_id(book.id_)
-        scroll_into_view(book_div)
-        book_div.find_element_by_class_name('book_title').click()
-
-        # Switch to Reader frame
-        self._browser.switch_to_default_content()
-        self._switch_to_frame('KindleReaderIFrame')
-
-        # Sync page to furthest position if further position was found
-        sync_btn_id = 'kindleReader_dialog_syncPosition_sync_btn'
-        self._wait.until(lambda br: br.find_element_by_id(sync_btn_id))
-        sync_btn = self._browser.find_element_by_id(sync_btn_id)
-        if sync_btn.is_displayed():
-            sync_btn.click()
-        self._wait.until_not(lambda br: sync_btn.is_displayed())
+        # Go to the reader page for `book`
+        self._to_book_reader(book)
 
         # Ensure header and footer are in view by bringing the search bar into
-        # focus (presses CMD+f)
-        self._action.key_down(Keys.COMMAND)\
-                    .send_keys('f')\
-                    .key_up(Keys.COMMAND)\
-                    .perform()
+        # focus
+        # NOTE: Wait required here because of intermittent errors raised by
+        # nsIDOMWindowUtils.sendKeyEvent
+        wait = WebDriverWait(self._browser,
+                        timeout=10,
+                        ignored_exceptions=(WebDriverException,))
+        find_action = self._action.key_down(Keys.COMMAND)\
+                        .send_keys('f')\
+                        .key_up(Keys.COMMAND)
+        wait.until_not(lambda br: find_action.perform())
 
         # Extract progress counters
-        footer = self._browser.find_element_by_id('kindleReader_footer_message')
-        progress_text = footer.text
-        dummy_percent, page_text, loc_text = progress_text.split(u' \xb7 ')
-        page_tuple = map(int, page_text.lstrip(u'Page ').split(u' of '))
-        loc_tuple = map(int, loc_text.lstrip(u'Location ').split(u' of '))
+        footer_id = 'kindleReader_footer_message'
+        self._wait.until(lambda br: br.find_element_by_id(footer_id).text)
+        progress_text = self._browser.find_element_by_id(footer_id).text
+        def _extract_pairs(pair_regex, text):
+            """If `pair_regex` matches `text`, return the pair of integers matched
+            Else if not match is found, return None
+            """
+            match = re.search(pair_regex, text)
+            return map(int, match.group(1, 2)) if match else None
+        page_tuple = _extract_pairs(ur'Page (\d+) of (\d+)', progress_text)
+        loc_tuple = _extract_pairs(ur'Location (\d+) of (\d+)', progress_text)
+
+        # Return to the library page
+        self._browser.find_element_by_id('kindleReader_button_close').click()
 
         return page_tuple, loc_tuple
 
@@ -236,7 +299,9 @@ if __name__ == "__main__":
         raise RuntimeError('Non-OS X platforms not supported')
 
     CREDENTIAL_PATH = '.credentials.json'
-    READER = KindleCloudReader(CREDENTIAL_PATH, get_ff_profile_path())
-    FIRST = READER.get_library()
-    print READER.get_current_progress(FIRST[0])
+    READER = KindleCloudReader(CREDENTIAL_PATH)
+    BOOKS = READER.get_library()
+    for BOOK in BOOKS:
+        PGS, LOCS = READER.get_current_progress(BOOK)
+        print PGS, BOOK.title
     READER.close()
