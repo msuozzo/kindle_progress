6bebd2ad949fa5d76ecd25683e72621de4534f58
Matthew Suozzo <matthew.suozzo@gmail.com>
2015-08-10 14:50:31 -0400

    Added data class for position, lots of refactoring

--- reader.py
+++ reader.py
@@ -11,10 +11,8 @@ import re
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver import Firefox, ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException,\
-                                        ElementNotVisibleException,\
-                                        WebDriverException
+                                        ElementNotVisibleException
 
 
 class ConnectionError(Exception):
@@ -65,6 +63,62 @@ class KindleBook(object):
         return u'Book(id={}, title="{}", author="{}")'\
                 .format(self.id_, self.title, self.author)\
                 .encode('utf8')
+
+
+class ReadingProgress(object):
+    """A representation of far the reader is through a book
+
+    Args:
+        book: The book for which the progress is associated
+        loc_pair: A 2-tuple (current_location, end_location)
+        page_pair (optional): A 2-tuple (current_page, end_page)
+
+
+    Notes on Progress Formats:
+
+    Page Numbers:
+        The page number measurement directly corresponds to the page
+        numbers in a physical copy of the book. In other words, the page
+        number N reported by the Kindle should correspond to that same
+        page N in a hard copy.
+
+    Locations:
+        According to (http://www.amazon.com/forum/kindle/Tx2S4K44LSXEWRI)
+        and various other online discussions, a single 'location' is
+        equivalent to 128 bytes of code (in the azw3 file format).
+
+        For normal books, this ranges from 3-4 locations per page with a
+        large font to ~16 locs/pg with a small font. However, book
+        elements such as images or charts may require many more bytes and,
+        thus, locations to represent.
+
+        In spite of this extra noise, locations provide a more granular
+        measurement of reading progress than page numbers.
+
+        Additionally, _locations are always available_ while page numbers are
+        frequently absent from Kindle metadata.
+    """
+    def __init__(self, book, loc_pair, page_pair=None):
+        self.book = book
+        self.current_loc, self.end_loc = loc_pair
+        if page_pair:
+            self.current_page, self.end_page = page_pair  #pylint: disable=unpacking-non-sequence
+        else:
+            self.current_page, self.end_page = (None, None)
+
+    def has_page_progress(self):
+        """Return whether page numbering is available in this object
+        """
+        return self.current_page is not None
+
+    def __repr__(self):
+        if self.has_page_progress():
+            return 'Progress(Loc=(%d of %d), Page=(%d of %d))' % \
+                    (self.current_loc, self.end_loc,
+                            self.current_page, self.end_page)
+        else:
+            return 'Progress(Loc=(%d of %d))' % \
+                    (self.current_loc, self.end_loc)
 
 
 class KindleCloudReader(object):
@@ -146,21 +200,63 @@ class KindleCloudReader(object):
 
     def _sync_reader(self):
         """Sync the reader position to the furthest available
+
+        When a Reader is launched, KCR will check to see whether a further
+        reading position exists on any other Kindle devices/apps.
+        If one is found, a 'load-in sync dialog' will be presented to user
+        with the option to sync with this further position.
+
+        Unfortunately, the book pane will usually load prior to a load-in sync
+        dialog prompt (if there is one). I couldn't find a suitable event to
+        wait for so one of the few alternatives would have been sleeping for a
+        second or two. This was uncomfortably inexact.
+
+        To make the process more deterministic, we immediately trigger a
+        manual sync when the book pane loads. This way, regardless of whether
+        or not a load-in sync dialog appears, we can process the sync.
+
+        Basically:
+            If the load-in sync dialog has loaded, we click through it.
+            Else, we trigger the sync and click through it just the same.
         """
-        # Sync page to furthest position if further position was found
-        # NOTE: book_container indicates the reader loaded without sync alert
-        sync_btn_id = 'kindleReader_dialog_syncPosition_sync_btn'
-        sync_or_read = lambda br: br.find_elements_by_id(sync_btn_id) or \
-                br.find_elements_by_id('kindleReader_book_container')
-        self._wait.until(sync_or_read)
+        self._wait.until(lambda br:\
+                br.find_element_by_id('kindleReader_book_container'))
+
+        self._reveal_reader_header_footer()
+
+        sync_pane_class = 'ui-dialog'
+        has_dialog = lambda br:\
+            bool(br.find_elements_by_class_name(sync_pane_class))
+        if not has_dialog(self._browser):
+            self._browser.find_element_by_id('kindleReader_button_sync').click()
+            self._wait.until(has_dialog)
 
         # Check whether sync is required
-        sync_btn_elems = self._browser.find_elements_by_id(sync_btn_id)
-        if sync_btn_elems:
-            sync_btn = sync_btn_elems[0]
-            if sync_btn.is_displayed():
-                sync_btn.click()
-            self._wait.until_not(lambda br: sync_btn.is_displayed())
+        sync_pane = self._browser.find_element_by_class_name(sync_pane_class)
+        buttons = sync_pane.find_elements_by_tag_name('button')
+        load_in_sync = len(buttons) == 3  # 3 buttons in load-in sync dialog
+        if load_in_sync:
+            dialogue_text = self._browser\
+                    .find_element_by_id('kindleReader_dialog_syncPosition_label').text
+            match = re.search(\
+                        ur'currently at .+ (\d+)\. .+ furthest .+ is (\d+) from',
+                        dialogue_text)
+            current, furthest = map(int, match.group(1, 2))
+            position_changed = current != furthest
+        requires_sync = load_in_sync and position_changed
+
+        # Record pre-sync progress
+        get_progress_text = lambda br: br\
+                    .find_element_by_id('kindleReader_footer_message').text
+        progress_text = get_progress_text(self._browser)
+
+        # If no sync available, buttons[0] will be the cancel button
+        # If there is a sync, buttons[0] will be 'Sync to Furthest Position'
+        self._wait.until_not(lambda br: buttons[0].click() or buttons[0].is_displayed())
+
+        # Wait for position to change
+        if requires_sync:
+            self._wait.until(lambda br: progress_text != get_progress_text(br))
 
     def _sync_library(self):
         """Sync the Kindle Cloud Library
@@ -198,6 +294,19 @@ class KindleCloudReader(object):
         self._wait.until(lambda br: br.find_element_by_id(frame_id))
         self._browser.switch_to.frame(frame_id)  #pylint: disable=no-member
 
+    def _reveal_reader_header_footer(self):
+        """Bring the header and footer into view by transferring focus to the
+        search bar.
+        """
+        def _set_active_and_check(browser):
+            """Set the searchbox as active and return whether the footer is
+            visible.
+            """
+            browser.execute_script('this.KindleReaderSearchBox.setActive(!0);')
+            footer = browser.find_element_by_id('kindleReader_footer_message')
+            return bool(footer.text)
+        self._wait.until(_set_active_and_check)
+
     def get_library(self):
         """Return metadata on the books in the kindle library
 
@@ -224,33 +333,11 @@ class KindleCloudReader(object):
         return books
 
     def get_current_progress(self, book):
-        """Return the current progress as 2 2-tuples:
-            ((curr_page, total_pages), (curr_location, total_locations))
-        NOTE: If either the page numbers or location information are not
-            available, None will be returned in place of the corresponding
-            tuple.
-                e.g. (None, (curr_loc, total_loc))
+        """Return a `ReadingProgress` object containing the available progress
 
-        Notes on Progress Formats:
+        data.
+        NOTE: A summary of the two progress formats can be found in the
 
-        Page Numbers:
-            The page number measurement directly corresponds to the page
-            numbers in a physical copy of the book. In other words, the page
-            number N reported by the Kindle should correspond to that same
-            page N in a hard copy.
-
-        Locations:
-            According to (http://www.amazon.com/forum/kindle/Tx2S4K44LSXEWRI)
-            and various other online discussions, a single 'location' is
-            equivalent to 128 bytes of code (in the azw3 file format).
-
-            For normal books, this ranges from 3-4 locations per page with a
-            large font to ~16 locs/pg with a small font. However, book
-            elements such as images or charts may require a many more bytes
-            and, thus, locations to represent.
-
-            In spite of this extra noise, locations provide a more granular
-            measurement of reading progress than page numbers.
 
         Args:
             read_cb: The callback to open the reader for the target book
@@ -258,22 +345,12 @@ class KindleCloudReader(object):
         # Go to the reader page for `book`
         self._to_book_reader(book)
 
-        # Ensure header and footer are in view by bringing the search bar into
-        # focus
-        # NOTE: Wait required here because of intermittent errors raised by
-        # nsIDOMWindowUtils.sendKeyEvent
-        wait = WebDriverWait(self._browser,
-                        timeout=10,
-                        ignored_exceptions=(WebDriverException,))
-        find_action = self._action.key_down(Keys.COMMAND)\
-                        .send_keys('f')\
-                        .key_up(Keys.COMMAND)
-        wait.until_not(lambda br: find_action.perform())
+        # Ensure footer is visible
+        self._reveal_reader_header_footer()
 
-        # Extract progress counters
-        footer_id = 'kindleReader_footer_message'
-        self._wait.until(lambda br: br.find_element_by_id(footer_id).text)
-        progress_text = self._browser.find_element_by_id(footer_id).text
+        # Extract progress counters from the footer
+        progress_text = self._browser\
+                .find_element_by_id('kindleReader_footer_message').text
         def _extract_pairs(pair_regex, text):
             """If `pair_regex` matches `text`, return the pair of integers matched
             Else if not match is found, return None
@@ -286,7 +363,7 @@ class KindleCloudReader(object):
         # Return to the library page
         self._browser.find_element_by_id('kindleReader_button_close').click()
 
-        return page_tuple, loc_tuple
+        return ReadingProgress(book, loc_tuple, page_tuple)
 
     def close(self):
         """End the browser session
@@ -302,6 +379,6 @@ if __name__ == "__main__":
     READER = KindleCloudReader(CREDENTIAL_PATH)
     BOOKS = READER.get_library()
     for BOOK in BOOKS:
-        PGS, LOCS = READER.get_current_progress(BOOK)
-        print PGS, BOOK.title
+        PROG = READER.get_current_progress(BOOK)
+        print PROG, BOOK.title
     READER.close()
