5f4261e0f496b75ed786ba38ce02f5681bdb1dd8
Matthew Suozzo <matthew.suozzo@gmail.com>
2015-08-23 00:34:25 -0400

    Big refactor, moved functionality away from main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 """Define a command line interface for Kindle Progress tracking
 """
-from events import AddEvent, SetReadingEvent, SetFinishedEvent, ReadEvent, \
-                    UpdateEvent
+from events import SetReadingEvent, SetFinishedEvent
 from store import EventStore
-from snapshot import KindleLibrarySnapshot, ReadingStatus
-from kindle_api.reader import KindleCloudReaderAPI
+from manager import KindleProgressMgr
 
-from datetime import datetime
 import json
 
 
@@ -15,67 +12,58 @@ STORE_PATH = '.store.txt'
 CREDENTIAL_PATH = '.credentials.json'
 
 
-def _update(snapshot, books, progress):
-    """Calculate the event diffs between `snapshot` and the library
-    state passed as `books` and `progress`
-
-    Args:
-        snapshot: A ``KindleLibrarySnapshot`` instance representing the
-            snapshot against which diff events will be generated.
-        books: The result of ``KindleCloudReaderAPI.get_book_metadata``
-        progress: The result of ``KindleCloudReaderAPI.get_book_progress``
+def safe_raw_input(*args, **kwargs):
+    """Wrapper around the normal `raw_input` builtin that, when a
+    `KeyboardInterrupt` is raised, the exception is caught and None is
+    returned.
     """
-    new_events = []
-    for book in books:
-        try:
-            data = snapshot.data[book.asin]
-        except KeyError:
-            new_events.append(AddEvent(book.asin))
-        else:
-            if data['status'] == ReadingStatus.CURRENT:
-                change = progress[book.asin].locs[1] - data['progress']
-                if change > 0:
-                    new_events.append(ReadEvent(book.asin, change))
-    return new_events
+    try:
+        return raw_input(*args, **kwargs)
+    except KeyboardInterrupt:
+        return None
 
 
 def run():  #pylint: disable=too-many-locals
     """Execute the command loop
     """
     store = EventStore(STORE_PATH)
-    snapshot = KindleLibrarySnapshot(store.get_events())
-
-    update_event = UpdateEvent(datetime.now().replace(microsecond=0))
     with open(CREDENTIAL_PATH, 'r') as cred_file:
         creds = json.load(cred_file)
-    with KindleCloudReaderAPI.get_instance(creds['uname'], creds['pword']) as kcr:
-        current_books = kcr.get_library_metadata()
-        current_progress = kcr.get_library_progress()
-        new_events = _update(snapshot, current_books, current_progress)
+        uname, pword = creds['uname'], creds['pword']
+    mgr = KindleProgressMgr(store, uname, pword)
 
-    # Events are sorted such that, when applied in order, each event
-    # represents a logical change in state. That is, an event never requires
-    # future events' data in order to be parsed.
-    # e.g. All ADDs must go before START READINGs
-    #      All START READINGs before all READs
-    print 'Processing updates:'
-    store.record_event(update_event)
-    for event in sorted(new_events):
-        print '  ' + str(event)
-        store.record_event(event)
-        snapshot.process_event(event)
-    if not new_events:
+    print 'Detecting updates to Kindle progress:'
+    events = mgr.detect_events()
+    if events is None:
+        print 'Failed to retrieve Kindle progress updates'
+        return
+    elif not events:
         print '  No updates detected'
+    else:
+        for event in events:
+            print '  ' + str(event)
 
     print
     print 'Finished updating.'
     print 'Mark new books as \'reading\' or old books as \'read\'? (y/N)'
-    if raw_input('> ') != 'y':
-        return
+    if safe_raw_input('> ') == 'y':
+        _change_state_prompt(mgr)
+    mgr.commit_events()
 
+
+def _change_state_prompt(mgr):
+    """Runs a prompt to change the state of books.
+
+    Registers `Event`s with `mgr` as they are requested.
+
+    Args:
+        mgr: A `KindleProgressMgr` object with the `books` and `progress`
+            fields populated.
+    """
     cmd = ''
-    book_range = range(1, len(current_books) + 1)
-    ind_to_book = dict(zip(book_range, current_books))
+    book_range = range(1, len(mgr.books) + 1)
+    ind_to_book = dict(zip(book_range, mgr.books))
+    get_book = lambda cmd_str: ind_to_book[int(cmd_str.split()[1])]
     while cmd != 'q':
         print 'Books:'
         for i in book_range:
@@ -84,27 +72,25 @@ def run():  #pylint: disable=too-many-locals
         print '| start {#}   | Start reading book with index {#}'
         print '| finish {#}  | Finish reading book with index {#}'
         print '| q           | Quit'
-        cmd = raw_input('> ')
-        get_book = lambda cmd_str: ind_to_book[int(cmd_str.split()[1])]
-        if cmd.startswith('start '):
+        cmd = safe_raw_input('> ')
+        if cmd is None or cmd == 'q':
+            break
+        elif cmd.startswith('start '):
             book = get_book(cmd)
-            initial_progress = current_progress[book.asin].locs[1]
+            initial_progress = mgr.progress[book.asin].locs[1]
             event = SetReadingEvent(book.asin, initial_progress)
         elif cmd.startswith('finish '):
             event = SetFinishedEvent(get_book(cmd).asin)
         else:
+            print 'Invalid command'
             event = None
         if event is not None:
             print
             print 'REGISTERED EVENT:'
             print '  ' + str(event)
-            store.record_event(event)
-            snapshot.process_event(event)
+            mgr.register_events((event))
         print
 
 
 if __name__ == '__main__':
-    try:
-        run()
-    except KeyboardInterrupt:
-        pass
+    run()
--- snapshot.py
+++ snapshot.py
@@ -7,7 +7,23 @@ from events import AddEvent, SetReadingEvent, SetFinishedEvent, ReadEvent, \
 class ReadingStatus(object):
     """An enum representing the three possible progress states of a book
     """
-    COMPLETED, CURRENT, NOT_STARTED = xrange(3)
+    NOT_STARTED, CURRENT, COMPLETED = xrange(3)
+
+
+class BookSnapshot(object):
+    """A book's state of progress
+
+    Args:
+        asin: The ASIN of the book
+        status: The book's ReadingStatus value
+        progress: An integral value representing the current reading progress.
+            This value is meaningless unless `status` is CURRENT as progress
+            is untracked for books not currently being read.
+    """
+    def __init__(self, asin, status=ReadingStatus.NOT_STARTED, progress=None):
+        self.asin = asin
+        self.status = status
+        self.progress = progress
 
 
 class KindleLibrarySnapshot(object):
@@ -18,7 +34,7 @@ class KindleLibrarySnapshot(object):
             to build the snapshot's state.
     """
     def __init__(self, events=()):
-        self.data = {}
+        self._data = {}
         for event in events:
             self.process_event(event)
 
@@ -28,16 +44,46 @@ class KindleLibrarySnapshot(object):
         if not isinstance(event, KindleEvent):
             pass
         elif isinstance(event, AddEvent):
-            self.data[event.asin] = {
-                    'status': ReadingStatus.NOT_STARTED,
-                    'progress': None
-                }
+            self._data[event.asin] = BookSnapshot(event.asin)
         elif isinstance(event, SetReadingEvent):
-            self.data[event.asin]['status'] = ReadingStatus.CURRENT
-            self.data[event.asin]['progress'] = event.initial_progress
+            self._data[event.asin].status = ReadingStatus.CURRENT
+            self._data[event.asin].progress = event.initial_progress
         elif isinstance(event, ReadEvent):
-            self.data[event.asin]['progress'] += event.progress
+            self._data[event.asin].progress += event.progress
         elif isinstance(event, SetFinishedEvent):
-            self.data[event.asin]['status'] = ReadingStatus.COMPLETED
+            self._data[event.asin].status = ReadingStatus.COMPLETED
         else:
             raise TypeError
+
+    def get_book(self, asin):
+        """Return the `BookSnapshot` object associated with `asin`
+
+        Raises:
+            KeyError: If asin not found in current snapshot
+        """
+        return self._data[asin]
+
+    def calc_update_events(self, asin_to_progress):
+        """Calculate and return an iterable of `KindleEvent`s which, when applied to
+        the current snapshot, result in the the current snapshot reflecting
+        the progress state of the `asin_to_progress` mapping.
+
+        Functionally, this method generates `AddEvent`s and `ReadEvent`s from
+        updated Kindle Library state.
+
+        Args:
+            asin_to_progress: A map of book asins to the integral
+                representation of progress used in the current snapshot.
+        """
+        new_events = []
+        for asin, new_progress in asin_to_progress.iteritems():
+            try:
+                book_snapshot = self.get_book(asin)
+            except KeyError:
+                new_events.append(AddEvent(asin))
+            else:
+                if book_snapshot.status == ReadingStatus.CURRENT:
+                    change = new_progress - book_snapshot.progress
+                    if change > 0:
+                        new_events.append(ReadEvent(asin, change))
+        return new_events
